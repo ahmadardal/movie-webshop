d44edec6b202c17bde8b53ada9ce9a7fbb72c240
Toni <toniwincent83@gmail.com>
2022-04-19 14:30:32 +0200

    useEffect stopped working so wrote function inside useEffect and now it works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   const [listOfMovies, setListOfMovies] = useState(null);
 
-  useEffect(getData, [])
+  useEffect(() => {
 
+    
+    async function getData() {
 
-  async function getData() {
-
-    console.log("GET DATA RUNNING!!!")
-
-    const moviesApi = 'http://www.omdbapi.com/?apikey=d18ade2a&s=batman';
-    const respons = await fetch(moviesApi);
-    const data = await respons.json();
-
-    await setListOfMovies(data);
-
-    console.log(listOfMovies);
-
-  }
-
+      console.log("GET DATA RUNNING!!!")
   
+      const moviesApi = 'http://www.omdbapi.com/?apikey=d18ade2a&s=batman';
+      const respons = await fetch(moviesApi);
+      const data = await respons.json();
+  
+      await setListOfMovies(data);
+  
+      console.log(listOfMovies);
+  
+    }
+    getData()
+  }, [])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
