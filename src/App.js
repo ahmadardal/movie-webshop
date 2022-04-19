ed2427103b529b919021efcce2a8aa67db4f00d1
nureddinelmas <nureddinelmas34@gmail.com>
2022-04-19 11:41:41 +0200

    push testat

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello jag är nu Nureddin</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <p>WELCOME to Movie Shop Project</p>
+
       </header>
     </div>
   );
