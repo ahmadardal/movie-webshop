7292bf985f5ea2684b1989731014d0828b9c986f
Toni <toniwincent83@gmail.com>
2022-04-19 14:23:56 +0200

    api fetch function working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [listOfMovies, setListOfMovies] = useState(null);
+
+  useEffect(getData, [])
+
+
+  async function getData() {
+
+    console.log("GET DATA RUNNING!!!")
+
+    const moviesApi = 'http://www.omdbapi.com/?apikey=d18ade2a&s=batman';
+    const respons = await fetch(moviesApi);
+    const data = await respons.json();
+
+    await setListOfMovies(data);
+
+    console.log(listOfMovies);
+
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
