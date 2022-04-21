534e964af3b7f7924c31a3e9f937a82faa74632e
nureddinelmas <nureddinelmas34@gmail.com>
2022-04-21 11:13:55 +0200

    rättat vissa filename

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Nav from './components/nav'
-import FullMovie from './components/fullmovie';
+import Nav from './components/Nav'
+import FullMovie from './components/Fullmovie';
 
 const App = () => {
   return (
