2293b5fa3bdcdf70dc381c39634f18b99cee89b5
Sanna Johansson <susanna.johansson@iths.se>
2022-04-22 11:47:19 +0200

    Api query title

--- src/components/GetMovieData.js
+++ src/components/GetMovieData.js
@@ -2,7 +2,7 @@ async function GetMovieData(setListOfMovies, title) {
 
     console.log("GET DATA RUNNING!!!")
 
-    const moviesApi = 'https://api.themoviedb.org/3/movie/640?api_key=357fd9dbde100c5f47082ae6a7f86d8b';
+    const moviesApi = 'https://api.themoviedb.org/3/search/movie?api_key=357fd9dbde100c5f47082ae6a7f86d8b&query=' + title;
     const respons = await fetch(moviesApi);
     const data = await respons.json();
 
