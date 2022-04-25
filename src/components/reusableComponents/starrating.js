dd1c1ae29ca39adfa5f51cf5448aeb34a06b2c8d
Toni <toniwincent83@gmail.com>
2022-04-25 14:41:32 +0200

    added int rating to the left

--- src/components/reusableComponents/starrating.js
+++ src/components/reusableComponents/starrating.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "../../styles/starrating.css";
 
-const StarRating = () => {
+const StarRating = (currentRating) => {
 
-    const [rating, setRating] = useState(0);
+    // This is temporary, this will be a int and set int function we take in from the moviecard component
+    const [rating, setRating] = useState(2);
+
     const [hover, setHover] = useState(0);
 
     return (
       <div className="star-rating">
+          <p>{rating}</p>
         {[...Array(5)].map((star, index) => {
           index += 1;
           return (
@@ -28,3 +31,14 @@ const StarRating = () => {
   };
 
 export default StarRating;
+
+/* 
+
+Set rating from start from the Int                                  [x]
+Even up the Int so that it can be converted to a star               []
+Do a average int to the left of the stars                           []
+Take in Int rating from object.vote_average                         []
+Change setRating to a function you take in (setRating from object)  []
+Find a way to show half a star                                      []
+
+*/
