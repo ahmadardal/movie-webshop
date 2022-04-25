3e066737184d0a8a029f7e321da787475b9f833d
crystalii <77482900+crystalii@users.noreply.github.com>
2022-04-25 11:01:03 +0200

    Fixes

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Button from "./reusableComponents/Button";
+import Button from "./reusableComponents/button";
 
 const Search = () => {
 
