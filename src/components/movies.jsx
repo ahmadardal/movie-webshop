a4d5065842867e75684b07984d25b1b3646bd4d0
Toni <toniwincent83@gmail.com>
2022-04-25 14:01:12 +0200

    star ratings created, need to add number rating

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import StarRating from "./reusableComponents/starrating";
 
 const Movies = () => {
 
     return (
         <div className="movies">
             <p>All movies are here</p>
+            <StarRating />
         </div>
     )
 }
