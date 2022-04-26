e9e87d9b80ffe597704209821fa9cf8bd2df9574
Toni <toniwincent83@gmail.com>
2022-04-26 14:43:45 +0200

    removed test star

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,13 +3,9 @@ import StarRating from "./reusableComponents/starrating";
 
 const Movies = () => {
 
-    // Switch me for the movie rating
-    const rating = 3.4;
-
     return (
         <div className="movies">
             <p>All movies are here</p>
-            <StarRating movieRating ={rating} />
         </div>
     )
 }
