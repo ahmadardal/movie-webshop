82426edb8ee484221e11cd0c28e16ba20da59f97
Sanna Johansson <susanna.johansson@iths.se>
2022-05-04 15:15:41 +0200

    Imported FeedbackCard

--- src/components/SelectedMovie.jsx
+++ src/components/SelectedMovie.jsx
@@ -1,10 +1,27 @@
-
+import "../styles/SelectedMovie.css";
+import StarRating from "./reusableComponents/starrating";
+import FeedbackCard from "./FeedbackCard";
 
 const SelectedMovie = () => {
 
     return (
-        <div className="selected-movie-container">
-            <h1>Hej hej!!</h1>
+        <div className="selected-movie-container">  
+            <div className="background-image-container">      
+               <img className="background-image-sm" src={require("../assets/img2.jpg")} />
+            </div>
+            <div className="selected-movie-info">
+                <p>Title</p>
+                <StarRating movieRating={2.7} size={'small-star-rating'} />
+                <p>Price: 47 kr</p>
+                <button className="button-add">Lägg till</button>
+            </div>
+            <div className="movie-info">
+                <p>Här ska det tas in info om filmen</p>
+            </div>
+            <div className="feedback-info">
+                <h1>Recensioner</h1>
+                <FeedbackCard />
+            </div>
         </div>
     )
 }
