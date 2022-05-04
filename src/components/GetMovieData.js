981c3e92a41ae68201b69689904b5c0b3dce22c4
Toni <89582846+Noccis@users.noreply.github.com>
2022-05-04 14:28:10 +0200

    made temp list for movies

--- src/components/GetMovieData.js
+++ src/components/GetMovieData.js
@@ -1,14 +1,16 @@
-async function GetMovieData(setListOfMovies, title) {
+async function GetMovieData(title) {
 
   console.log("GET DATA RUNNING!!!")
 
+  /* let searchResults = []; */
   const moviesApi = 'https://api.themoviedb.org/3/search/movie?api_key=357fd9dbde100c5f47082ae6a7f86d8b&query=' + title;
   const respons = await fetch(moviesApi);
   const data = await respons.json();
   // kommentar
-  await setListOfMovies(data);
+  /* await setListOfMovies(data); */
+  
   console.log(data);
-
+  return data;
 }
 
 export default GetMovieData;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StarRating from "./reusableComponents/starrating";
+import GetMovieData from '../components/GetMovieData';
 
 const Movies = () => {
+    const [tempMovies, setTempMovies] = useState(null);
+    let listOfMovies = null;
+
+
+    // Removie this code when redux is installed
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const moviesApi = 'https://api.themoviedb.org/3/search/movie?api_key=357fd9dbde100c5f47082ae6a7f86d8b&query=batman';
+            const respons = await fetch(moviesApi);
+            const data = await respons.json();
+            setTempMovies(data);
+        }
+
+        fetchData();
+        
+    }, [])
+
+
+    if(tempMovies != null) {
+        listOfMovies = tempMovies.results.map(movie => (
+            <div key={movie.id} className="moviesMovieContainer">
+                <p>{movie.title}</p>
+            </div>
+        )); 
+    }
+
+
 
     return (
         <div className="discoverMoviesContainer">
-            <p>All movies are here</p>
+           {listOfMovies}
         </div>
     )
 }
+
 
 export default Movies;
