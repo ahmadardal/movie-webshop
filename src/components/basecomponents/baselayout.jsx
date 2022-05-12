b460516e77ffe3362496034dbf4dee6d80b63170
Toni <89582846+Noccis@users.noreply.github.com>
2022-05-12 15:14:55 +0200

    remodelling

--- src/components/basecomponents/baselayout.jsx
+++ src/components/basecomponents/baselayout.jsx
@@ -8,10 +8,51 @@ import MovieSlider from "../movieslider";
 import BurgerMenyIcon from "../../assets/hamburger.png"
 import ShoppingCartContainer from "../shoppingCart/shoppingCartContainer";
 import FeedbackForm from "../FeedbackForm";
+import Home from "../../pages/home";
 
 const BaseLayout = ({ headerContent, mainContent, footerContent }) => {
-  const [showMobileMenu, setShowMobileMenu] = useState(false);
   
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [hiddenMenu, setHiddenMenu] = useState(false);
+  const [itemCount, setItemCount] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(10);
+  const [cartItems, setCardItems] = useState([
+    {movieName: "Batman", img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRBkxgl2A2PhE_6tklFLT0bxn5NLhvhsnpXGhmXBt_zotrlVHmo', price: 10, movieId: 1, cartCount: 2},
+     {movieName: "Batman", img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRBkxgl2A2PhE_6tklFLT0bxn5NLhvhsnpXGhmXBt_zotrlVHmo', price: 20, movieId: 2, cartCount: 3},
+     {movieName: "Batman", img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRBkxgl2A2PhE_6tklFLT0bxn5NLhvhsnpXGhmXBt_zotrlVHmo', price: 30, movieId: 3, cartCount: 5},
+     {movieName: "Batman", img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRBkxgl2A2PhE_6tklFLT0bxn5NLhvhsnpXGhmXBt_zotrlVHmo', price: 50, movieId: 6, cartCount: 7}]);
+
+  const updateCart = (movieName,movieId, movieImg, moviePrice) => {
+
+      setItemCount(prevCount => prevCount + 1)
+      setTotalPrice(prevPrice => prevPrice + moviePrice)
+  
+      if (cartItems.find(item => item.id === movieId)) {
+        const result = cartItems.find(item => item.id === movieId);
+        result.cartCount++;
+        return;
+      }
+      setCardItems([...cartItems,
+      { movieName: movieName,img: movieImg, price: moviePrice, movieId: movieId, cartCount: 1 }
+      ])
+  }
+
+  const removeItem = (movieId) => {
+    const newItem = cartItems.filter(cartItem => cartItem.movieId !== movieId);
+    setCardItems(newItem);
+
+    const itemCountResult = newItem.reduce(
+      (acc, val) => (acc += val.cartCount), 0
+    );
+    setItemCount(itemCountResult);
+
+    const priceResult = newItem.reduce(
+      (acc, val) => (acc += val.price * val.cartCount), 0
+    );
+
+    setTotalPrice(priceResult)
+  }
+
   return (
     <div className="base-layout-all-content-container">
       <img 
@@ -29,20 +70,32 @@ const BaseLayout = ({ headerContent, mainContent, footerContent }) => {
         <div />
       )}
 
+      <div className="menuWrapper" onClick={() => setHiddenMenu(!hiddenMenu)}>
+        <p>{itemCount > 0 && itemCount}</p>
+        <i className={hiddenMenu ? "fas fa-times fa-lg" : "fas fa-shopping-cart fa-lg"}></i>
+        <div id="ShoppingCartMenu">
+          {hiddenMenu ? <ShoppingCartContainer
+            hiddenMenu={hiddenMenu}
+            cartItems={cartItems}
+            itemCount={itemCount}
+            removeItem={removeItem}
+            totalPrice={totalPrice} /> : ""} 
+        </div>
+      </div>
+
       <div className="Sidebar">  
         <MenuList />
       </div> 
 
+{/* Allt som ska vara på sidan som inte är sidebar, meny eller shoppingcart ska vara här */}
       <div className="header-and-main-content-container">
         <div className="margin">
-        <div className="headContent">433
-          <Search />
-
-        </div>
-        
+          <div className="headContent">
+            <Search />
+          </div>
+        {/* Här kommer komponenterna vi byter ut vara */}
         <div className="mainContent" style={{ marginTop: 30 }}>
-        {mainContent}
-       
+            <Home />
         </div> 
         
         <div className="footerContent">{footerContent}</div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import FeedbackCard from "../components/FeedbackCard";
 import FeedbackForm from "../components/FeedbackForm";
 import Button from "../components/reusableComponents/button";
 import { actions } from "../state/reducers/counterReducer.js";
-import ShoppingCartContainer from "../components/shoppingCart/shoppingCartContainer";
+
 import Movies from "../components/movies";
 import MovieCard from "../components/MovieCard";
 import MovieSlider from "../components/movieslider";
@@ -16,7 +16,7 @@ import "../styles/Home.css"
 
 const Home = (props) => {
 
-  const { hiddenMenu, cartItems, totalPrice, itemCount, removeItem, setHiddenMenu } = props;
+  
 
   const value = useSelector((state) => state.counter);
   const dispatch = useDispatch();
@@ -30,38 +30,15 @@ const Home = (props) => {
 
   return (
     <div className="homeBaseContainer">
-      
-      
-      <div className="menuWrapper" onClick={() => setHiddenMenu(!hiddenMenu)}>
-        <p>{itemCount > 0 && itemCount}</p>
-        <i className={hiddenMenu ? "fas fa-times fa-lg" : "fas fa-shopping-cart fa-lg"}></i>
-       <div id="ShoppingCartMenu">
-        {hiddenMenu ? <ShoppingCartContainer
-          hiddenMenu={hiddenMenu}
-          cartItems={cartItems}
-          itemCount={itemCount}
-          removeItem={removeItem}
-          totalPrice={totalPrice} /> : ""} 
-      </div>
-      </div>
-      <BaseLayout
-        headerContent={
-          <div></div>
-        }
-        mainContent={
-          <div>
-            <h1>{value}</h1>
-            <MovieSlider />
-            <Movies />
-            <Button btnText={"Hello"} handleClick={btnPress} />
-            <FeedbackCard feedback="Hej" author="Ahmad Ardal" age="18" />
-            <FeedbackForm />
-            <FeedbackCard feedback="Hej" author="Ahmad Ardal" age="18" />
-          </div>
-        }
-      />
+        <h1>{value}</h1>
+        <MovieSlider />
+        <Movies />
+        {/* <Button btnText={"Hello"} handleClick={btnPress} />
+        <FeedbackCard feedback="Hej" author="Ahmad Ardal" age="18" />
+        <FeedbackForm />
+        <FeedbackCard feedback="Hej" author="Ahmad Ardal" age="18" /> */}
+   
     </div>
-
   );
 };
 
