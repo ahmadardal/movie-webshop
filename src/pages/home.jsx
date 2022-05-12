b762f9929d395981108c7c00f7c303b42768d236
nureddinelmas <nureddinelmas34@gmail.com>
2022-05-12 11:46:10 +0200

    Shopping CArt

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -28,6 +28,7 @@ const Home = (props) => {
   return (
     <div>
       {/* Shopping Cart */}
+      <div></div>
         <div className="menuWrapper" onClick={() => setHiddenMenu(!hiddenMenu)}>
           <p>{itemCount > 0 && itemCount}</p>
           <i className={hiddenMenu ? "fas fa-times fa-lg" : "fas fa-shopping-cart fa-lg"} ></i>
