329e1ae2628f2b441c7e883fe52a0340359c5049
Sanna Johansson <susanna.johansson@iths.se>
2022-05-17 16:01:04 +0200

    movieSlider fixed

--- src/components/movieslider.jsx
+++ src/components/movieslider.jsx
@@ -1,49 +1,35 @@
 import "../styles/movieSlider.css";
 import { MdChevronLeft,MdChevronRight } from "react-icons/md";
-import Dots from 'react-carousel-dots';
 import MovieCard from "./MovieCard"
 import { Link } from "react-router-dom";
-const movieSlider = () => {
+import React, { useEffect, useState } from "react";
 
-  const slides = [
-    {
-      image: 'https://picsum.photos/200/300',
-      title: "Här har vi den allra första bilden", 
-      rating:5,       
-  },
-  {
-      image:'https://picsum.photos/100/300',
-      title: "Här har vi den andra bilden", 
-      rating: 7,
-  },
-  {
-      image:'https://picsum.photos/200/400',
-      title: "Här har vi den tredje bilden",
-      rating: 3,
-  },
-  {
-    image: 'https://picsum.photos/100/400',
-    title: "Här har vi den fjärde bilden", 
-    rating:5,       
-},
-{
-  image: 'https://picsum.photos/500/300',
-  title: "Här har vi den femte bilden", 
-  rating:6,       
-},
-{
-  image: 'https://picsum.photos/600/500',
-  title: "Här har vi den sjätte bilden", 
-  rating:5,       
-},
-{
-  image: 'https://picsum.photos/100/400',
-  title: "Här har vi den sjunde bilden", 
-  rating:5,       
-},
-  ];
+const MovieSlider = () => {
 
-  const movie = ''; // Gör en map av slides i return delen och skicka in en movie
+  const [slides, setSlides] = useState(null);
+  let sliderList = null;
+
+  useEffect(() => {
+  const fetchData = async () => {
+    const moviesApi =
+      "https://api.themoviedb.org/3/discover/movie?primary_release_year=2020&api_key=357fd9dbde100c5f47082ae6a7f86d8b&sort_by=vote_average.desc";
+    const respons = await fetch(moviesApi);
+    const data = await respons.json();
+    setSlides(data);
+  };
+
+  fetchData()
+}, []);
+
+if (slides != null) {
+  sliderList = slides.results.map((movie) => (
+    <div className="slider-card" key={movie.id}> 
+      <Link to="/SelectedMovie" state={{ movie: movie}}>       
+          <MovieCard movie={movie}/>        
+      </Link> 
+    </div>
+  ));
+}
 
   const slideLeft = () => {
       var slider = document.getElementById("slider");
@@ -55,39 +41,17 @@ const movieSlider = () => {
     slider.scrollLeft = slider.scrollLeft + 500;
   }
 
-  // const cardClicked = () => {
-  //   alert("You clicked the card");
-  // }
-
-  const cardClicked = () => {
-    <Link></Link> ;
-  }
-
   return (
     <div className="main-slider-container">
-      <MdChevronLeft size={40} className="slider-icon left" onClick={slideLeft}/>   
-      
-      <div id="slider">
-      
-  
-       { slides.map((slide,index) => {
-          return (
-            <Link to="/SelectedMovie">          
-            <div className="slider-card" key={index}>            
-
-            <MovieCard movie={slide}/>
-
-            </div>
-            </Link> 
-          )
-        })}
-          
-      </div>
+      <MdChevronLeft size={40} className="slider-icon left" onClick={slideLeft}/>
+        <div id="slider"> 
+          {sliderList}
+        </div>      
       <MdChevronRight size={40} className="slider-icon right" onClick={slideRight}/>
     </div>
   );
 }
 
-export default movieSlider
+export default MovieSlider
 
 
